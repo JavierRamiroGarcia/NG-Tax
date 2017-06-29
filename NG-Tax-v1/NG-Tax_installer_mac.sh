cbb897175ea4ccb1b8d3d787892dab25d28e162a
Javier Ramiro-Garcia <ramiro.garcia.javier@gmail.com>
2017-06-29 13:56:36 +0200

    Installer for mac

--- NG-Tax-v1/NG-Tax_installer_mac.sh
+++ NG-Tax-v1/NG-Tax_installer_mac.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 
+# Install brew, wget, gzip and clustalw
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install wget
+brew install gzip
+brew install clustalw
+
 # Download Silva databases
 
 mkdir SILVA_db
-cd db
+cd SILVA_db
 wget https://www.arb-silva.de/fileadmin/silva_databases/release_128/Exports/SILVA_128_SSURef_tax_silva.fasta.gz
-gunpzip SILVA_128_SSURef_tax_silva.fasta.gz
-wget wget https://www.arb-silva.de/fileadmin/silva_databases/release_128/Exports/taxonomy/tax_slv_ssu_128.txt
+gunzip SILVA_128_SSURef_tax_silva.fasta.gz
+wget https://www.arb-silva.de/fileadmin/silva_databases/release_128/Exports/taxonomy/tax_slv_ssu_128.txt
 
 # Add bin folder to PATH
 
@@ -19,10 +26,6 @@ echo -e 'export PATH="$PATH:'$path_to_NG_tax'"\n' >>  ~/.bashrc
 source  ~/.bashrc
 
 cd ../test_set
-
-# Install clustalw
-
-sudo apt-get install clustalw
 
 # Update changes
 
